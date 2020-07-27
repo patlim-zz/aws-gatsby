209333409609ed0ec30849b3f2c1181dde3b26b0
Pat-Lim <impatlim@gmail.com>
2020-07-28 11:53:55 +1200

    add aws services page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,13 +2,20 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import S3 from "../components/s3"
+import Codepipline from "../components/codepipeline"
+import Codebuild from "../components/codebuild"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="AWS Services" />
+    <h1>AWS Services</h1>
+     <div style={{ display: `grid`, gridTemplateColumns: `repeat(3, 1fr)`, marginBottom: `1.45rem` }}>
+      <S3 />
+      <Codepipline />
+      <Codebuild />
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
